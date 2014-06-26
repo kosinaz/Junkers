4bca23b174d4b97527441886970a8a7ad480893e
Kosina Zoltán <zoltan@kosina.hu>
2014-06-26 22:29:13 +0200

    JSDoc annotations added

--- game.js
+++ game.js
@@ -7,8 +7,8 @@
 
 /**
  * The main class of the game. Holds the objects of its graphical display, its
- * text window under it, its level data, its scheduler and engine. To start the
- * game, the init function has to be called.
+ * text area under it, its level data, its scheduler and engine. To start the
+ * game, its init function has to be called.
  * @constructor
  */
 var Game = function () {
@@ -23,45 +23,55 @@ var Game = function () {
   this.TEXT_HEIGHT = 3;
 };
 
+/**
+ * Initializes the loading of the game. After the game window has loaded, the
+ * handleEvent function continues the initialization.
+ */
 Game.prototype.init = function () {
   "use strict";
   window.addEventListener("load", this);
 };
 
+/**
+ * Finishes the initialization of the game. Creates the display, sets its size
+ * to full window. Creates the text area, the scheduler, the engine, the level.
+ * Creates the player and the NPC-s, adds them to the scheduler, and finally
+ * starts the engine.
+ * @param {event.type} e The load event.
+ */
 Game.prototype.handleEvent = function (e) {
   "use strict";
   var i;
-  switch (e.type) {
-  case "load":
-    window.removeEventListener("load", this);
-    this.display = new ROT.Display({
-      width: this.WIDTH,
-      height: this.HEIGHT + this.TEXT_HEIGHT,
-      fontFamily: "Arial"
-    });
-    document.body.appendChild(this.display.getContainer());
-    this.display.setOptions({
-      fontSize: this.display.computeFontSize(
-        screen.availWidth,
-        screen.availHeight
-      )
-    });
-    this.text = new TextBuffer(this.display, this.HEIGHT, this.TEXT_HEIGHT);
-    this.scheduler = new ROT.Scheduler.Speed();
-    this.engine = new ROT.Engine(this.scheduler);
-    this.level = new Level(this.WIDTH, this.HEIGHT);
-    this.level.setEntity(new Player("⇑⇗⇒⇘⇓⇙⇐⇖"));
-    for (i = 0; i < 10; i += 1) {
-      this.level.setEntity(new Being("⇧⬀⇨⬂⇩⬃⇦⬁"));
-    }
-    for (i in this.level.beings) {
-      if (this.level.beings.hasOwnProperty(i)) {
-        this.scheduler.add(this.level.beings[i], true);
-      }
-    }
-    this.engine.start();
-    break;
+  if (e.type !== "load") {
+    return;
   }
+  window.removeEventListener("load", this);
+  this.display = new ROT.Display({
+    width: this.WIDTH,
+    height: this.HEIGHT + this.TEXT_HEIGHT,
+    fontFamily: "Arial"
+  });
+  document.body.appendChild(this.display.getContainer());
+  this.display.setOptions({
+    fontSize: this.display.computeFontSize(
+      screen.availWidth,
+      screen.availHeight
+    )
+  });
+  this.text = new TextBuffer(this.display, this.HEIGHT, this.TEXT_HEIGHT);
+  this.scheduler = new ROT.Scheduler.Speed();
+  this.engine = new ROT.Engine(this.scheduler);
+  this.level = new Level(this.WIDTH, this.HEIGHT);
+  this.level.setEntity(new Player("⇑⇗⇒⇘⇓⇙⇐⇖"));
+  for (i = 0; i < 10; i += 1) {
+    this.level.setEntity(new Being("⇧⬀⇨⬂⇩⬃⇦⬁"));
+  }
+  for (i in this.level.beings) {
+    if (this.level.beings.hasOwnProperty(i)) {
+      this.scheduler.add(this.level.beings[i], true);
+    }
+  }
+  this.engine.start();
 };
 
 Game.prototype.over = function () {
